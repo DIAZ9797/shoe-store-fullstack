f1b3c3be6c03eb6286c36a658121dc8991e8dd11
M Dicky Azkiaa <dickyazkia9797a@gmailh.com>
2026-01-25 14:04:57 +0700

    Matikan protected route total

--- shoe-store/src/components/ProtectedRoute.js
+++ shoe-store/src/components/ProtectedRoute.js
@@ -1,17 +1,8 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
 
+// SATPAM YANG TIDUR (Meloloskan siapa saja)
 const ProtectedRoute = ({ children }) => {
-  // Cek apakah ada 'token' user yang tersimpan di Local Storage
-  // (Asumsi saat login Anda menyimpan token dengan nama "token" atau "user")
-  const token = localStorage.getItem("token") || localStorage.getItem("user");
-
-  if (!token) {
-    // Kalau tidak ada token, paksa pindah ke Login
-    return <Navigate to="/login" replace />;
-  }
-
-  // Kalau ada token, silakan masuk (tampilkan halaman yang dituju)
+  // Langsung return children tanpa cek token/alert apapun
   return children;
 };
 
